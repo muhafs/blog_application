09279d104d2c7b65fc0f572f4903eebb349ed2bb
muhamadhaspin <muhamadhaspin@gmail.com>
2022-11-15 12:12:21 +0700

    redesign post detail page

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,15 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         \App\Models\User::factory(10)->create();
-        \App\Models\Category::factory(10)->create();
+
+        $categories = ['Web Programming', 'Personal', 'Video Games', 'Business'];
+        foreach ($categories as $category) {
+            \App\Models\Category::create([
+                'name' => $category,
+                'slug' => str($category)->slug('-')
+            ]);
+        }
+
         \App\Models\Post::factory(10)->create();
 
         // \App\Models\User::factory()->create([
--- resources/js/Pages/Post/Show.vue
+++ resources/js/Pages/Post/Show.vue
@@ -9,12 +9,14 @@ defineProps(["post"]);
     <Head title="Muhamad Blog - Blog" />
 
     <GuestLayout>
-        <article class="mb-4 rounded-lg bg-white p-3 shadow-lg">
-            <h2 class="text-xl font-bold text-primary">
+        <article class="mx-auto w-8/12 rounded-lg bg-white p-4 shadow-lg">
+            <h2 class="mb-6 text-center text-3xl font-bold text-slate-900">
                 {{ post.title }}
             </h2>
 
-            <h5 class="mb-3 text-sm font-semibold text-slate-500">
+            <h5
+                class="mb-3 flex items-center justify-between text-sm font-semibold text-slate-500"
+            >
                 <span>
                     By :
                     <Link href="#" class="text-primary">{{
@@ -32,13 +34,18 @@ defineProps(["post"]);
                 </span>
             </h5>
 
-            <p class="mt-4 text-base font-medium text-slate-700">
+            <img
+                src="https://via.placeholder.com/600x200"
+                class="w-full rounded-sm object-cover shadow-md"
+            />
+
+            <p class="mt-4 text-base font-light text-slate-700">
                 {{ post.content }}
             </p>
 
             <Link
                 :href="route('blog')"
-                class="mt-4 inline-block text-slate-500"
+                class="mt-4 inline-block rounded-md bg-primary px-3 py-1 text-white"
             >
                 « back
             </Link>
